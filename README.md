b19814ea010cee237dcc292f84551d3d25a9bdde
unknown <yjqpro@gmail.com>
2021-11-19 15:24:04 +0800

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+# generator-vcpkg-cmake [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> 
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-vcpkg-cmake using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-vcpkg-cmake
+```
+
+Then generate your new project:
+
+```bash
+yo vcpkg-cmake
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+Apache-2.0 © [yjqpro]()
+
+
+[npm-image]: https://badge.fury.io/js/generator-vcpkg-cmake.svg
+[npm-url]: https://npmjs.org/package/generator-vcpkg-cmake
+[travis-image]: https://travis-ci.com/yjqpro/generator-vcpkg-cmake.svg?branch=master
+[travis-url]: https://travis-ci.com/yjqpro/generator-vcpkg-cmake
+[daviddm-image]: https://david-dm.org/yjqpro/generator-vcpkg-cmake.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/yjqpro/generator-vcpkg-cmake
---
+++ generators/app/index.js
@@ -0,0 +1,108 @@
+'use strict';
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  // Constructor
+  
+  constructor (args, opts)
+  {
+    
+    super (args, opts) ;
+
+    this.config.getAll() ;
+
+    this.config.defaults
+    (
+      {
+        projectName: 'C++ Project',
+        projectPath: "CppProject"
+      }
+    );
+
+    this.interactive = true ;
+
+
+    this.argument('name', { type: String, required: false }) ;
+
+    if (this.options.name)
+    {
+      this.config.set('projectName', this.options.name) ;
+      this.config.set('projectPath', this.options.name) ;
+      this.interactive = false ;
+    }
+
+  }
+
+  prompting ()
+  {
+
+    if (!this.interactive)
+    {
+      return ;
+    }
+
+
+    return this.prompt
+    (
+      [
+        {
+          type: 'input',
+          name: 'projectName',
+          message: 'Project name:',
+          default: this.config.get('projectName')
+        },
+        {
+          type: 'input',
+          name: 'projectPath',
+          message: 'Project path:',
+          default: this.config.get('projectPath')
+        }
+      ]
+    ).then(
+      (answers) =>
+      {
+        this.config.set('projectName', answers['projectName']);
+        this.config.set('projectPath', answers['projectPath']);
+      }
+    );
+  }
+
+  write() 
+  {
+    this._setupBuild();
+  }
+
+  _setupBuild() 
+  {
+    this.fs.copyTpl
+    (
+      this.templatePath('CMakeLists.txt'),
+      this.destinationPath('CMakeLists.txt'),
+      {
+        projectName: this.config.get('projectName'),
+        projectPath: this.config.get('projectPath')
+      }
+    );
+
+    this.fs.copyTpl
+    (
+      this.templatePath("vcpkg.json"),
+      this.destinationPath("vcpkg.json"),
+      {
+        projectName: this.config.get('projectName')
+      }
+    );
+
+    this.fs.copy
+    (
+      this.templatePath("CppProject/main.cc"),
+      this.destinationPath("src/" + this.config.get("projectPath") + "/main.cc")
+    );
+    
+    this.fs.copy
+    (
+      this.templatePath("CppProject/CMakeLists.txt"),
+      this.destinationPath("src/" + this.config.get("projectPath") + "/CMakeLists.txt")
+    );
+  }
+};
